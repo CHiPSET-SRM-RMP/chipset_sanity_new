8fca56839759d9f7382f84f0cd22f5173d965178
Nikhil Sharma <79254517+nikhilsharma1574@users.noreply.github.com>
2025-08-17 14:13:25 +0530

    Google Analytics error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/layouts/Navbar";
 import Footer from "@/components/Reusable/Footer";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 import Head from "next/head";
+import Script from "next/script";
 export const metadata: Metadata = {
   title: "CHIPSET",
   description: "CHIPSET | SRM UNIVERSITY RAMAPURAM",
@@ -22,22 +22,18 @@ Readonly<{children: React.ReactNode;}>)
       <title>CHIPSET</title>
       <meta name="CHIPSET | SRM UNIVERSITY RAMAPURAM" />
       <link rel="icon" href="/favicon.ico" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-90LSQWVPVQ"></Script>
+      <Script id="google-analytics">
+        {
+          `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-90LSQWVPVQ');`
+        }
+      </Script>
     </Head>
     <html lang="en" className="scrollbar-none overflow-y-scroll">
       <body className={`${inter.className}`}>
-{/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-90LSQWVPVQ"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-90LSQWVPVQ');
-          `}
-        </Script>
         <Navbar/>
           {children}
         <Footer/>
